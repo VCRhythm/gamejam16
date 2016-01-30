7d2cd2539f8d0614da1a13415867d4bf687dca8e
Diego Ruiz <cvillian098@gmail.com>
2016-01-29 23:00:18 -0500

    title screen minimal requirements commit

---
+++ Assets/Scripts/TitleQuitBtn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class TitleQuitBtn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void OnClicked()
+    {
+        Debug.Log("we would quit here2");
+        Application.Quit();
+    }
+    
+}
