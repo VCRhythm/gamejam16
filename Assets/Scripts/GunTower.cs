6110cb441cbffd85af2d08ed45e65b89ebbeac28
cody burchett <cburchet@gmu.edu>
2016-01-29 22:08:30 -0500

    added towers
    towers

---
+++ Assets/Scripts/GunTower.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunTower : Tower {
+
+    private float nextFire;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //generic stats to be changed
+        health = 100;
+        damage = 15;
+        range = 5;
+        fireRate = 1f;
+        upgradeDamage = 3;
+        upgradeHealth = 10;
+        maxLevel = 5;
+        target = null;
+        nextFire = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //check if tower has target
+	    if (!target)
+        {
+            //get a new target
+            target = GetTarget();
+        }
+        else
+        {
+            //check if target is out of range
+            if ((target.transform.position - transform.position).magnitude > range)
+            {
+                //remove out of range target
+                target = null;
+            }
+            else
+            {
+                //fire at target
+                if (nextFire < Time.time)
+                {
+                    Fire();
+                    nextFire = Time.time + fireRate;
+                }
+            }
+        }
+	}
+
+    override protected GameObject GetTarget()
+    {
+        GameObject closestTarget;
+        Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, range, enemyLayer);
+        if (enemiesInRange.Length > 0)
+        {
+            //get closest enemy
+            closestTarget = enemiesInRange[0].gameObject;
+            for (int i = 0; i < enemiesInRange.Length; i++)
+            {
+                GameObject nextTarget = enemiesInRange[i].gameObject;
+                if ((nextTarget.transform.position - transform.position).magnitude < (closestTarget.transform.position - transform.position).magnitude)
+                {
+                    closestTarget = nextTarget;
+                }
+            }
+            return closestTarget;
+        }
+        return null;
+    }
+
+    //add to stats if upgraded
+    override public void Upgrade()
+    {
+        level++;
+        health += upgradeHealth;
+        damage += upgradeDamage;
+    }
+
+    protected override void Fire()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, target.transform.position - transform.position, out hit, 5, enemyLayer);
+        if (hit.collider.gameObject)
+        {
+            Debug.Log("Hit enemy");
+            //add damage to enemy
+        }
+    }
+}
---
+++ Assets/Scripts/WallTower.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallTower : Tower {
+
+	// Use this for initialization
+	void Start () {
+        health = 10;
+        damage = 15;
+        upgradeDamage = 3;
+        upgradeHealth = 10;
+        maxLevel = 5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    override protected GameObject GetTarget()
+    {
+        return null;
+    }
+
+    override public void Upgrade()
+    {
+        level++;
+        health += upgradeHealth;
+        damage += upgradeDamage;
+    }
+
+    void OnColliderEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("enemy");
+            health--;
+            //deal damage to enemy and push it back
+            other.transform.Translate((other.transform.position - transform.position) * 2);
+        }
+        if (health == 0)
+        {
+            Destroy(this);
+        }
+    }
+}
