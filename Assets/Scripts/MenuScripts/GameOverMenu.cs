a7a5f18726d73a84489e0b1a0f06963301563efa
Diego Ruiz <cvillian098@gmail.com>
2016-01-30 21:37:42 -0500

    Game Over/Win Screens

---
+++ Assets/Scripts/MenuScripts/GameOverMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class GameOverMenu : MonoBehaviour {
+    GameObject PersistenceGameObject;
+	// Use this for initialization
+	void Start () {
+        if (GameObject.FindGameObjectsWithTag("PersistentObject").Length>0)
+        {
+            PersistenceGameObject = GameObject.FindGameObjectWithTag("PersistentObject");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ResetLevel() {
+        DontDestroyOnLoad(PersistenceGameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void returnToMainMenu()
+    {
+        DontDestroyOnLoad(PersistenceGameObject);
+        SceneManager.LoadScene(0);
+    }
+}
